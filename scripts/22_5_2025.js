7337e20236d48d070126dba7139e30544cea5a42
Akarsh A S <akarshas@Akarsh-MacBook-Pro.local>
2025-05-22 10:53:44 +0530

    22/5/2025

---
+++ scripts/22_5_2025.js
@@ -0,0 +1,50 @@
+// console.log('22_5_2025');
+
+// Null and undefined
+
+// let a = null;
+// console.log(a); // null
+// console.log(a + 'akarsh'); // nullakarsh
+
+// a = 1;
+// console.log(a); // 1
+
+// let b;
+// console.log(b); // undefined
+
+// b = 1;
+// console.log(b); // 1
+
+// console.log(b + 'akarsh'); // undefined
+
+// let a = 'akarsh@gmail.com';
+// console.log(a.includes('@')); // true
+
+// Comparisons
+
+// console.log(1 == 1);
+// console.log(1 == '1');
+// console.log(1 === '1');
+
+// Type conversion
+
+// let a = '1';
+// let b = Number(a);
+// let c = +a;
+// console.log(a);
+// console.log(b);
+// console.log(b === c);
+
+// Loops
+
+//FOR LOOP
+// for (let i = 0; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// WHILE LOOP
+let number = prompt('Enter a number greater than 10:');
+
+while (number <= 10) {
+  number = prompt('Too small! Enter a number greater than 10:');
+}
